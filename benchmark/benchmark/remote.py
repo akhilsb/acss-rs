67d0595165ba8904c6704c62beaa97b5fe1ac1d3
Akhil <akhilsai2712@gmail.com>
2025-01-19 22:11:22 -0500

    low threshold evaluation

--- benchmark/benchmark/remote.py
+++ benchmark/benchmark/remote.py
@@ -246,7 +246,7 @@ class Bench:
         Print.info('Booting primaries...')
         st_time = round(time.time() * 1000) + 60000
         batches = 1
-        per_batch = 10000
+        per_batch = 1700
         exp_vals = self.exp_setup(4)
         import numpy as np
         tri = np.max(exp_vals) - np.min(exp_vals)
