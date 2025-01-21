bd62d810ea6a0a66cf6fb7f571bb5d7bf98a1d58
Akhil <akhilsai2712@gmail.com>
2025-01-21 17:38:25 -0500

    high threshold

--- benchmark/benchmark/remote.py
+++ benchmark/benchmark/remote.py
@@ -246,7 +246,7 @@ class Bench:
         Print.info('Booting primaries...')
         st_time = round(time.time() * 1000) + 60000
         batches = 1
-        per_batch = 230
+        per_batch = 92
         exp_vals = self.exp_setup(4)
         import numpy as np
         tri = np.max(exp_vals) - np.min(exp_vals)
@@ -352,7 +352,7 @@ class Bench:
         Print.info('Booting primaries...')
         st_time = round(time.time() * 1000) + 60000
         batches = 1
-        per_batch = 460
+        per_batch = 92
         for i,ip in enumerate(hosts):
             #host = Committee.ip(address)
             if i == 0:
